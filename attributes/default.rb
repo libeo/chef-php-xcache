2c811bc78bdcaf5adf9104088b44a390ef02b2f3
Jakub Riedl <jakub.riedl@le.cz>
2014-09-25 16:00:54 +0200

    enable better configuration and multiple ini paths for fpm

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,23 +1,42 @@
-default['xcache']['extension'] = 'xcache.so'
-default['xcache']['admin.auth'] = 'On'
-default['xcache']['admin.user'] = 'admin'
-default['xcache']['admin.pass'] = ''
-default['xcache']['test'] = 'Off'
-default['xcache']['cacher'] = 'On'
-default['xcache']['size'] = '16M'
-default['xcache']['count'] = 1
-default['xcache']['slots'] = '8K'
-default['xcache']['ttl'] = 0
-default['xcache']['gc_interval'] = 300
-default['xcache']['stat'] = 'On'
-default['xcache']['var_size'] = '0M'
-default['xcache']['var_count'] = 1
-default['xcache']['var_slots'] = '8K'
-default['xcache']['var_ttl'] = 0
-default['xcache']['var_maxttl'] = 0
-default['xcache']['var_gc_interval'] = 300
-default['xcache']['readonly_protection'] = 'Off'
-default['xcache']['mmap_path'] = '/dev/zero'
-default['xcache']['optimizer'] = 'Off'
-default['xcache']['coverager'] = 'Off'
-default['xcache']['shm_scheme'] = 'mmap'
+case node['platform']
+	when 'centos'
+		default['php-xcache']['conf_dirs'] = ['/etc/php.d']
+		default['php-xcache']['conf_file'] = 'xcache.ini'
+		default['php-xcache']['package'] = 'php-xcache'
+	when 'debian'
+		default['php-xcache']['conf_dirs'] = ['/etc/php5/conf.d']
+		default['php-xcache']['conf_file'] = '20-xcache.ini'
+		default['php-xcache']['package'] = 'php5-xcache'
+	when 'ubuntu'
+		default['php-xcache']['package'] = 'php5-xcache'
+		default['php-xcache']['conf_file'] = 'xcache.ini'
+		if node['platform_version'] < '14.04'
+			default['php-xcache']['conf_dirs'] = ['/etc/php5/conf.d']
+		else
+			default['php-xcache']['conf_dirs'] = ['/etc/php5/mods-available']
+		end
+end
+
+default['php-xcache']['xcache']['extension'] = 'xcache.so'
+default['php-xcache']['xcache']['admin.auth'] = 'On'
+default['php-xcache']['xcache']['admin.user'] = 'admin'
+default['php-xcache']['xcache']['admin.pass'] = ''
+default['php-xcache']['xcache']['test'] = 'Off'
+default['php-xcache']['xcache']['cacher'] = 'On'
+default['php-xcache']['xcache']['size'] = '16M'
+default['php-xcache']['xcache']['count'] = 1
+default['php-xcache']['xcache']['slots'] = '8K'
+default['php-xcache']['xcache']['ttl'] = 0
+default['php-xcache']['xcache']['gc_interval'] = 300
+default['php-xcache']['xcache']['stat'] = 'On'
+default['php-xcache']['xcache']['var_size'] = '0M'
+default['php-xcache']['xcache']['var_count'] = 1
+default['php-xcache']['xcache']['var_slots'] = '8K'
+default['php-xcache']['xcache']['var_ttl'] = 0
+default['php-xcache']['xcache']['var_maxttl'] = 0
+default['php-xcache']['xcache']['var_gc_interval'] = 300
+default['php-xcache']['xcache']['readonly_protection'] = 'Off'
+default['php-xcache']['xcache']['mmap_path'] = '/dev/zero'
+default['php-xcache']['xcache']['optimizer'] = 'Off'
+default['php-xcache']['xcache']['coverager'] = 'Off'
+default['php-xcache']['xcache']['shm_scheme'] = 'mmap'
--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          'All rights reserved'
 description      'Installs php-xcache'
 #Problemes d'accent, alors ne pas mettre le README comme description longue
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 depends          'php'
 depends          'yum-repoforge'
